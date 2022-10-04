2b2e706e2d6ac3660c5b1c84d60a1055adf9151c
VladUs <vladustyugo@gmail.com>
2022-10-04 17:48:15 +0700

    selection sort

--- algorithms-and-structures/Sorting.js
+++ algorithms-and-structures/Sorting.js
@@ -9,5 +9,18 @@ const bubbleSort = (arr) => {
   return arr;
 }
 
+const selectionSort = arr => {
+  for(let wall = arr.length - 1; wall > 0; wall--){
+    let indexLargest = 0;
+    for(let i = 1; i <= wall; i++){
+      if(arr[i] > arr[indexLargest]){
+        indexLargest=i
+      }
+    }
+    [arr[wall], arr[indexLargest]] = [arr[indexLargest], arr[wall]]
+  }
+  return arr;
+}
 const arr = [23, 41, 25, 54, 18, 14];
-console.log(bubbleSort(arr))
+console.log(bubbleSort(arr));
+console.log(selectionSort(arr));
