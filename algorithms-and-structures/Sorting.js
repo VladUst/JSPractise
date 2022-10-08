d2581246db9444810437a01bfb9f447c01e864bf
VladUs <vladustyugo@gmail.com>
2022-10-08 12:32:03 +0700

    shell sort

--- algorithms-and-structures/Sorting.js
+++ algorithms-and-structures/Sorting.js
@@ -34,7 +34,24 @@ const insertionSort = arr => {
   return arr;
 }
 
+const shellSort = arr => {
+  let gap = 1;
+  while(gap < arr.length/3) {
+    gap = gap*3 + 1;
+  }
+  while(gap >= 1){
+    for(let i = gap; i < arr.length; i++){ // при gap = 1 фактически станет wall = 1 для вставок
+      for(let j = i; j >= gap && arr[j] < arr[j - gap]; j-=gap){
+        [arr[j], arr[j-gap]] = [arr[j-gap], arr[j]];
+      }
+    }
+    gap = Math.floor(gap / 3);
+  }
+  return arr;
+}
+
 const arr = [23, 41, 25, 54, 18, 14];
+console.log(shellSort(arr));
 console.log(insertionSort(arr));
 console.log(bubbleSort(arr));
 console.log(selectionSort(arr));
